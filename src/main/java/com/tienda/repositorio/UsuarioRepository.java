3e02108c925ade95309c78022680b43a1df96944
MiguelMenTic <mhmendozat@gmail.com>
2026-01-18 20:20:51 -0500

    Se implemento la clase Repositorio

---
+++ src/main/java/com/tienda/repositorio/UsuarioRepository.java
@@ -0,0 +1,15 @@
+// Paquete donde se ubicarán tus repositorios
+package com.tienda.repositorio;
+
+import com.tienda.modelo.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCorreo(String correo);
+
+    Optional<Usuario> findByDni(String dni);
+}
