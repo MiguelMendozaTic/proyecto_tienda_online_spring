2ff9ed688716c8f4768ef4b9e6e5d71dc205ebe0
Miguel <mhmendozat@gmail.com>
2026-02-22 16:44:11 -0500

    Correccion 3 Jenkinsfile

--- src/test/java/com/tienda/TiendaApplicationTests.java
+++ src/test/java/com/tienda/TiendaApplicationTests.java
@@ -2,8 +2,10 @@ package com.tienda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;  // ¡Agrega esta importación!
 
 @SpringBootTest
+@ActiveProfiles("test")  // ¡Agrega esta línea!
 class TiendaApplicationTests {
 
 	@Test
